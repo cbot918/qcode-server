9bdd8e2b73365fc671530e888453f7069e119443
cbot918 <cbot918@gmail.com>
2023-06-15 16:55:57 +0800

    update

--- src/main/java/yale/code/Controller.java
+++ src/main/java/yale/code/Controller.java
@@ -3,26 +3,26 @@ package yale.code;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import yale.code.util.Util;
 
 import java.sql.* ;  // for standard JDBC programs
-import java.math.* ; // for BigDecimal and BigInteger support
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class Controller {
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@CrossOrigin("*")
 	public String hello(){
 		System.out.println("request in");
 		return "Hey, Spring Boot 的 Hello World ! ";
 	}
 	
-	
-	@RequestMapping("/db/ping")
+	@RequestMapping(value = "/db/ping", method = RequestMethod.GET)
 	@CrossOrigin("*")
 	public String db() {
 
@@ -60,10 +60,11 @@ public class Controller {
 
 		return "db ping successfully";
 	}
-	
-	@RequestMapping("/db/insert")
+
+
+	@RequestMapping(value = "/echo_request", method = RequestMethod.POST)
 	@CrossOrigin("*")
-	public ResponseEntity<Book> dbInsert() {
+	public ResponseEntity<Book> echoRequest(@RequestBody Book book) {
 
 		String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 		String host = "localhost:3306";
@@ -79,28 +80,76 @@ public class Controller {
 		Connection conn = null;
 		try { conn = DriverManager.getConnection(connString, username,password); } 
 		catch (SQLException e) { e.printStackTrace(); }
-		
-		// create statement
+
+
+		// // create statement
+		// String sql = String.format(
+		// 	"INSERT INTO book(title, isbn, language) VALUES('%s','%s','%s')",
+		// 	book.getTitle(),book.getIsbn(),book.getLanguage()
+		// );
+
 		// try {
 		// 	Statement stmt = conn.createStatement();
-
-		// 	String sql = "INSERT INTO users VALUES('testname')";
 
 		// 	stmt.executeUpdate(sql);
 			
 		// 	System.out.println("row inserted");
 
 		// } catch (SQLException e) { e.printStackTrace(); }
-		
 
-		Book book = new Book("test book title", 123, "chinese");
-
-
+		Book retBook = new Book(book.getTitle(), book.getIsbn(), book.getLanguage());
 		
 		// connection close
 		try { conn.close(); } catch (SQLException e) { e.printStackTrace(); }
 
-		return ResponseEntity.ok().body(book);
+		return ResponseEntity.ok().body(retBook);
+	}
+
+
+	@RequestMapping(value = "/book/create", method = RequestMethod.POST)
+	@CrossOrigin("*")
+	public ResponseEntity<Book> createBook(@RequestBody Book book) {
+
+		String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+		String host = "localhost:3306";
+		String db = "qcode";
+		String username = "root";
+		String password = "12345";
+		String connString = String.format("jdbc:mysql://%s/%s", host,db);
+
+		// init jdbc_driver
+		Util.initDb(DB_DRIVER);
+		
+		// init connection
+		Connection conn = null;
+		try { conn = DriverManager.getConnection(connString, username,password); } 
+		catch (SQLException e) { e.printStackTrace(); }
+
+
+		// create statement
+		String sql = String.format(
+			"INSERT INTO book(title, isbn, language) VALUES('%s','%s','%s')",
+			book.getTitle(),book.getIsbn(),book.getLanguage()
+		);
+
+		try {
+			Statement stmt = conn.createStatement();
+
+			stmt.executeUpdate(sql);
+			
+			System.out.println("row inserted");
+
+		} catch (SQLException e) { e.printStackTrace(); }
+
+
+
+
+		Book retBook = new Book(book.getTitle(), book.getIsbn(), book.getLanguage());
+		
+		// connection close
+		try { conn.close(); } catch (SQLException e) { e.printStackTrace(); }
+
+		return ResponseEntity.ok().body(retBook);
 	}
 
 }
